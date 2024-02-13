ae71a715d97ce38745fe9cc6ed24827102648aa4
Dominik Fűrst <dominikfurst1@gmail.com>
2024-02-13 16:17:25 +0100

    Init

---
+++ src/test/java/com/upce/nnpia/NnpiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upce.nnpia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
